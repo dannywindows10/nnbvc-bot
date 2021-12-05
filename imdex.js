6dfd9ac5bc4066eb0a1d1bf0ee0c743efa4ac788
dannywindows10 <95439182+dannywindows10@users.noreply.github.com>
2021-12-05 12:11:30 +0100

    dd

---
+++ imdex.js
@@ -0,0 +1,9 @@
+const didiscord = reqvler(`discord.js`)
+const bot = new discord.client()
+const TOKEN = `OTE2NDI2Nzc0NTE3MjgwNzg4.Yap-8Q.3G49WbBhwbrfRV9vKD0vKDOpa7I`
+const prefix = `nnbvc`
+
+
+    
+
+    
